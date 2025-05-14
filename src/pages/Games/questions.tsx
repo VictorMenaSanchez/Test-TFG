dce61927642f48e5c94626ab069267c26c918322
victor <vmenasanchez@gmail.com>
2025-05-14 19:00:51 +0200

    fallo

---
+++ src/pages/Games/questions.tsx
@@ -0,0 +1,26 @@
+// src/questions.ts
+export const questions = [
+  {
+    id: 1,
+    question: "¿Cuántas horas duerme un gato al día?",
+    options: ["4-6", "12-16", "18-20", "22-24"],
+    correctAnswer: "12-16",
+  },
+  {
+    id: 2,
+    question: "¿Qué significa cuando un gato amasa con sus patas?",
+    options: [
+      "Está cazando",
+      "Está enojado",
+      "Está cómodo y feliz",
+      "Tiene hambre",
+    ],
+    correctAnswer: "Está cómodo y feliz",
+  },
+  {
+    id: 3,
+    question: "¿Cómo se llama el sonido que hacen los gatos al estar contentos?",
+    options: ["Maullido", "Ronroneo", "Chillido", "Aullido"],
+    correctAnswer: "Ronroneo",
+  },
+];
